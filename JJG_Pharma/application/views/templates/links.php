5ff5e43847960ad7a9e8dbb994f31afd3933b212
gamhongnd-84929 <55821877+gamhongnd-84929@users.noreply.github.com>
2020-04-01 11:49:41 -0700

    Update links.php

--- JJG_Pharma/application/views/templates/links.php
+++ JJG_Pharma/application/views/templates/links.php
@@ -1,3 +1,7 @@
-<d><a href="http://localhost/JJG_Pharma/index.php/search">Search</a></d>
+<!-- <d><a href="http://localhost/JJG_Pharma/index.php/search">Search</a></d>
 <d><a href="http://localhost/JJG_Pharma/index.php/login">Login</a></d>
-<d><a href="http://localhost/JJG_Pharma/index.php/profile">Profile</a></d>
+<d><a href="http://localhost/JJG_Pharma/index.php/profile">Profile</a></d> -->
+
+<a href="<?php echo base_url().'index.php/search'; ?>">Search</a>
+<a href="<?php echo base_url().'index.php/login'; ?>">Login</a>
+<a href="<?php echo base_url().'index.php/profile'; ?>">Profile</a>
