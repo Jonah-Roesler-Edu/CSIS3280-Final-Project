7b13c3727bad9b0f18d80cdb34a53100f326943a
gamhongnd-84929 <55821877+gamhongnd-84929@users.noreply.github.com>
2020-04-01 11:48:45 -0700

    Update profile.php

--- JJG_Pharma/application/views/pages/profile.php
+++ JJG_Pharma/application/views/pages/profile.php
@@ -1,11 +1,28 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="UTF-8">
-    <title>Profile</title>
-  </head>
-  <body>
-      <h1>Profile</h1>
-  
-  </body>
-</html>
+
+<h1>Profile</h1>
+<table>
+        <tr>
+            <td>User Name: </td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>First Name:........ </td>
+            <td>Last Name:......... </td>
+        </tr>
+        <tr>
+            <td>Item:......... </td>
+            
+        </tr>
+        <tr>
+            <td>Quantity: </td>
+            <td>Price/Unit: </td>
+            <td>Total:  </td>
+        </tr>
+        <tr>
+            <td>Date:......... </td> 
+        </tr>
+        
+        </table>
+        <form action="Lab09-TTR-84929-login.php" method="POST">
+            <input type="submit" value="Logout">
+        </form>
